1758f5ac355fdd410df5bedec7e3d64e9f88f6bd
krishamaemundo8-dev <krishamaemundo8@gmail.com>
2026-05-05 17:20:25 +0800

    Delete views directory

--- views/home.php
+++
@@ -1,6 +0,0 @@
-<section id="home" class="content active">
-    <div class="homecontent">
-        <h1 class="splash">Welcome to Student Manangement System</h1>
-        <p class="splash">A Project to Integrative Programming Technologiess</p>
-    </div>
-</section>
